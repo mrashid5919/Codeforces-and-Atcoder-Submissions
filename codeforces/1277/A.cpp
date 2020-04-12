283b6af6877abd828d4f53d8e8f542ece52cd56c
Mayesha Rashid <1905103@ugrad.cse.buet.ac.bd>
2020-04-12 14:30:00 +0600

    Add solution for problem `A - Happy Birthday, Polycarp!` Link: https://codeforces.com/contest/1277/problem/A Tags: implementation, *1000 Ref: https://codeforces.com/contest/1277/submission/76305312

---
+++ codeforces/1277/A.cpp
@@ -0,0 +1,161 @@
+//Happy birthday(incomplete)
+#include<stdio.h>
+int main()
+{
+    long long int t,n,i,j,a,b,c,d,e,f,g,h,k,sum;
+    scanf("%lld",&t);
+    for(i=0;i<t;i++)
+    {
+        scanf("%lld",&n);
+        if(n<10)
+            sum=n;
+        else if(n<11)
+            sum=9;
+        else if(n<22)
+            sum=10;
+        else if(n<33)
+            sum=11;
+        else if(n<44)
+            sum=12;
+        else if(n<55)
+            sum=13;
+        else if(n<66)
+            sum=14;
+        else if(n<77)
+            sum=15;
+        else if(n<88)
+            sum=16;
+        else if(n<99)
+            sum=17;
+        else if(n<111)
+            sum=18;
+        else if(n<222)
+            sum=19;
+        else if(n<333)
+            sum=20;
+        else if(n<444)
+            sum=21;
+        else if(n<555)
+            sum=22;
+        else if(n<666)
+            sum=23;
+        else if(n<777)
+            sum=24;
+        else if(n<888)
+            sum=25;
+        else if(n<999)
+            sum=26;
+        else if(n<1111)
+            sum=27;
+        else if(n<2222)
+            sum=28;
+        else if(n<3333)
+            sum=29;
+        else if(n<4444)
+            sum=30;
+        else if(n<5555)
+            sum=31;
+        else if(n<6666)
+            sum=32;
+        else if(n<7777)
+            sum=33;
+        else if(n<8888)
+            sum=34;
+        else if(n<9999)
+            sum=35;
+        else if(n<11111)
+            sum=36;
+        else if(n<22222)
+            sum=37;
+        else if(n<33333)
+            sum=38;
+        else if(n<44444)
+            sum=39;
+        else if(n<55555)
+            sum=40;
+        else if(n<66666)
+            sum=41;
+        else if(n<77777)
+            sum=42;
+        else if(n<88888)
+            sum=43;
+        else if(n<99999)
+            sum=44;
+        else if(n<111111)
+            sum=45;
+        else if(n<222222)
+            sum=46;
+        else if(n<333333)
+            sum=47;
+        else if(n<444444)
+            sum=48;
+        else if(n<555555)
+            sum=49;
+        else if(n<666666)
+            sum=50;
+        else if(n<777777)
+            sum=51;
+        else if(n<888888)
+            sum=52;
+        else if(n<999999)
+            sum=53;
+        else if(n<1111111)
+            sum=54;
+        else if(n<2222222)
+            sum=55;
+        else if(n<3333333)
+            sum=56;
+        else if(n<4444444)
+            sum=57;
+        else if(n<5555555)
+            sum=58;
+        else if(n<6666666)
+            sum=59;
+        else if(n<7777777)
+            sum=60;
+        else if(n<8888888)
+            sum=61;
+        else if(n<9999999)
+            sum=62;
+        else if(n<11111111)
+            sum=63;
+        else if(n<22222222)
+            sum=64;
+        else if(n<33333333)
+            sum=65;
+        else if(n<44444444)
+            sum=66;
+        else if(n<55555555)
+            sum=67;
+        else if(n<66666666)
+            sum=68;
+        else if(n<77777777)
+            sum=69;
+        else if(n<88888888)
+            sum=70;
+        else if(n<99999999)
+            sum=71;
+        else if(n<111111111)
+            sum=72;
+        else if(n<222222222)
+            sum=73;
+        else if(n<333333333)
+            sum=74;
+        else if(n<444444444)
+            sum=75;
+        else if(n<555555555)
+            sum=76;
+        else if(n<666666666)
+            sum=77;
+        else if(n<777777777)
+            sum=78;
+        else if(n<888888888)
+            sum=79;
+        else if(n<999999999)
+            sum=80;
+        else
+            sum=81;
+        printf("%lld\n",sum);
+    }
+    return 0;
+}
