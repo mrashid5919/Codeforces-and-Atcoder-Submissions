140f604c3500b62b8c54c5734f283b69f3a9802c
Mayesha Rashid <1905103@ugrad.cse.buet.ac.bd>
2020-08-15 23:28:00 +0600

    Add solution for problem `B - Making Triangle` Link: https://atcoder.jp/contests/abc175/tasks/abc175_b Tags: AtCoder, *200 Ref: https://atcoder.jp/contests/abc175/submissions/15969517

--- atcoder/abc175/B.cpp
+++ atcoder/abc175/B.cpp
@@ -5,11 +5,12 @@ using namespace std;
 int main()
 {
     fastio;
-    long long n,i,j,k,sum=0;
+    long long n,i,j,k,sum=0,high,low,mid,p;
     static long long ara[103];
     cin>>n;
     for(i=0;i<n;i++)
         cin>>ara[i];
+    sort(ara,ara+n);
     for(i=0;i<n-2;i++)
     {
         for(j=i+1;j<n-1;j++)
@@ -20,12 +21,30 @@ int main()
             {
                 for(k=j+1;k<n;k++)
                 {
-                    if(ara[j]==ara[k] || ara[i]==ara[k])
+                    if(ara[j]==ara[k])
                         continue;
                     else
                     {
-                        if(ara[i]+ara[j]>ara[k] && ara[i]+ara[k]>ara[j] && ara[j]+ara[k]>ara[i])
-                            sum++;
+                        low=k;
+                        high=n-1;
+                        p=0;
+                        while(low<=high)
+                        {
+                            mid=(low+high)/2;
+                            if(ara[mid]>=ara[i]+ara[j] && ara[mid-1]<ara[i]+ara[j])
+                            {
+                                sum=sum+mid-k;
+                                p=1;
+                                break;
+                            }
+                            else if(ara[mid]<ara[i]+ara[j])
+                                low=mid+1;
+                            else if(ara[mid]>=ara[i]+ara[j])
+                                high=mid-1;
+                        }
+                        if(p==0)
+                            sum=sum+n-k;
+                        break;
                     }
                 }
             }
