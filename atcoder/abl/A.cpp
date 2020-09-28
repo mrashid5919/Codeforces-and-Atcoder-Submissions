029182192ddbd10957459e8e2cd9ce4ae135291a
Mayesha Rashid <1905103@ugrad.cse.buet.ac.bd>
2020-09-28 17:57:00 +0600

    Add solution for problem `A - Repeat ACL` Link: https://atcoder.jp/contests/abl/tasks/abl_a Tags: AtCoder, *100 Ref: https://atcoder.jp/contests/abl/submissions/17084337

---
+++ atcoder/abl/A.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+#include<string>
+using namespace std;
+#define fastio ios_base::sync_with_stdio(false);cin.tie(NULL)
+
+int main()
+{
+    int k;
+    string s="ACL";
+    cin>>k;
+    while(k--)
+        cout<<s;
+    return 0;
+}
