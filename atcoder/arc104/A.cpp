86bb97aba693a413b39adf4571472fef802181f3
Mayesha Rashid <1905103@ugrad.cse.buet.ac.bd>
2020-10-03 18:05:00 +0600

    Add solution for problem `A - Plus Minus` Link: https://atcoder.jp/contests/arc104/tasks/arc104_a Tags: AtCoder, *100 Ref: https://atcoder.jp/contests/arc104/submissions/17159829

---
+++ atcoder/arc104/A.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define fastio ios_base::sync_with_stdio(false);cin.tie(NULL)
+
+int main()
+{
+    fastio;
+    int a,b;
+    cin>>a>>b;
+    cout<<(a+b)/2<<" "<<(a-b)/2<<"\n";
+    return 0;
+}
