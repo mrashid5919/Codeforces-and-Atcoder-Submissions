4bcdb4d6e7ce9d14fb5d41f6d708844321198230
Mayesha Rashid <1905103@ugrad.cse.buet.ac.bd>
2020-10-17 17:21:00 +0600

    Add solution for problem `A - box` Link: https://atcoder.jp/contests/abc180/tasks/abc180_a Tags: AtCoder, *100 Ref: https://atcoder.jp/contests/abc180/submissions/17454608

---
+++ atcoder/abc180/A.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define fastio ios_base::sync_with_stdio(false);cin.tie(NULL)
+
+int main()
+{
+    fastio;
+    int n,a,b;
+    cin>>n>>a>>b;
+    cout<<n-a+b<<"\n";
+    return 0;
+}
