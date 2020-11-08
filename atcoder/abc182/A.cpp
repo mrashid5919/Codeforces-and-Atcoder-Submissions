69c92ac20094e2459f99bd301c7352371f801d1d
Mayesha Rashid <1905103@ugrad.cse.buet.ac.bd>
2020-11-08 18:07:00 +0600

    Add solution for problem `A - twiblr` Link: https://atcoder.jp/contests/abc182/tasks/abc182_a Tags: AtCoder, *100 Ref: https://atcoder.jp/contests/abc182/submissions/17956945

---
+++ atcoder/abc182/A.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define fastio ios_base::sync_with_stdio(false);cin.tie(NULL)
+
+int main()
+{
+    fastio;
+    long long a,b,ans;
+    cin>>a>>b;
+    ans=(2*a)+100-b;
+    cout<<ans;
+    return 0;
+}
