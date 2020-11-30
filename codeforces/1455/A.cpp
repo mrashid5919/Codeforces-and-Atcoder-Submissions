ac886a5a3c2c8d669c5ec4543b2c5ecdfb682442
Mayesha Rashid <1905103@ugrad.cse.buet.ac.bd>
2020-11-30 20:40:00 +0600

    Add solution for problem `A - Strange Functions` Link: https://codeforces.com/contest/1455/problem/A Tags: math, number theory, *800 Ref: https://codeforces.com/contest/1455/submission/99998170

---
+++ codeforces/1455/A.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+#include<string>
+using namespace std;
+#define fastio ios_base::sync_with_stdio(false);cin.tie(NULL)
+
+int main()
+{
+    fastio;
+    string s;
+    int t,n;
+    cin>>t;
+    while(t--)
+    {
+        cin>>s;
+        n=s.size();
+        cout<<n<<"\n";
+    }
+    return 0;
+}
