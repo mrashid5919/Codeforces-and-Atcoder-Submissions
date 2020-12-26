766cd0aa6a356db5e850de77e35365ccda351e93
Mayesha Rashid <1905103@ugrad.cse.buet.ac.bd>
2020-12-26 19:22:00 +0600

    Add solution for problem `A - Brick` Link: https://atcoder.jp/contests/abc186/tasks/abc186_a Tags: AtCoder, *100 Ref: https://atcoder.jp/contests/abc186/submissions/18998518

---
+++ atcoder/abc186/A.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define fastio ios_base::sync_with_stdio(false);cin.tie(NULL)
+
+int main()
+{
+    fastio;
+    int n,w;
+    cin>>n>>w;
+    cout<<n/w<<"\n";
+    return 0;
+}
