1e04931b81d30dd0666051ad775f42748454ff8b
Mayesha Rashid <1905103@ugrad.cse.buet.ac.bd>
2021-03-21 19:32:00 +0600

    Add solution for problem `A - Prison Break` Link: https://codeforces.com/contest/1484/problem/A Tags: constructive algorithms, *800 Ref: https://codeforces.com/contest/1484/submission/110632965

---
+++ codeforces/1484/A.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define fastio ios_base::sync_with_stdio(false);cin.tie(NULL)
+
+int main()
+{
+    fastio;
+    int t,a,b;
+    cin>>t;
+    while(t--)
+    {
+        cin>>a>>b;
+        cout<<a*b<<"\n";
+    }
+    return 0;
+}
