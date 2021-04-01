68e014476b42278af213fb1762119f0ca901b436
Mayesha Rashid <1905103@ugrad.cse.buet.ac.bd>
2021-04-01 22:43:00 +0600

    Add solution for problem `A - Is it rated - 2` Link: https://codeforces.com/contest/1505/problem/A Tags: *special problem, implementation, interactive, *900 Ref: https://codeforces.com/contest/1505/submission/111724827

---
+++ codeforces/1505/A.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+#include<string>
+#include<fstream>
+using namespace std;
+#define fastio ios_base::sync_with_stdio(false);cin.tie(NULL)
+
+int main()
+{
+    fastio;
+    string s;
+    while(getline(cin,s))
+    {
+        if(s=="Is it rated?")
+            cout<<"NO"<<endl;
+    }
+    return 0;
+}
