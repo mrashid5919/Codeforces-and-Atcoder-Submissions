01f00e9d64ff21625a4b71b5ec7c23e39584fe5e
Mayesha Rashid <1905103@ugrad.cse.buet.ac.bd>
2021-04-11 18:01:00 +0600

    Add solution for problem `A - Div` Link: https://atcoder.jp/contests/abc198/tasks/abc198_a Tags: AtCoder, *100 Ref: https://atcoder.jp/contests/abc198/submissions/21636536

---
+++ atcoder/abc198/A.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define fastio ios_base::sync_with_stdio(false);cin.tie(NULL)
+
+int main()
+{
+    fastio;
+    int n;
+    cin>>n;
+    cout<<n-1<<"\n";
+    return 0;
+}
