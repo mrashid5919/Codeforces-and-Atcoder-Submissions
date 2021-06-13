ef6e2d9d8e9bbe68895c5011704ca63d436e4dd6
Mayesha Rashid <1905103@ugrad.cse.buet.ac.bd>
2021-06-13 18:32:00 +0600

    Add solution for problem `A - kcal` Link: https://atcoder.jp/contests/abc205/tasks/abc205_a Tags: AtCoder, *100 Ref: https://atcoder.jp/contests/abc205/submissions/23428809

---
+++ atcoder/abc205/A.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define fastio ios_base::sync_with_stdio(false);cin.tie(NULL)
+
+int main()
+{
+    fastio;
+    int a,b;
+    float ans;
+    cin>>a>>b;
+    ans=((float)a*b)/100;
+    cout<<ans<<"\n";
+    return 0;
+}
