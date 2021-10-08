3d386180513f896df3bb6dfe81c794af77be34a0
Mayesha Rashid <1905103@ugrad.cse.buet.ac.bd>
2021-10-08 21:11:00 +0600

    Add solution for problem `A - Consecutive Sum Riddle` Link: https://codeforces.com/contest/1594/problem/A Tags: math, *800 Ref: https://codeforces.com/contest/1594/submission/131174357

---
+++ codeforces/1594/A.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define fastio ios_base::sync_with_stdio(false);cin.tie(NULL)
+
+int main()
+{
+    fastio;
+    long long t,n;
+    cin>>t;
+    while(t--)
+    {
+        cin>>n;
+        cout<<(-1)*(n-1)<<" "<<n<<"\n";
+    }
+    return 0;
+}
