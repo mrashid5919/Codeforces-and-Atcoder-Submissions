ff6b3281a04d80001ffa4472b37c7efb930f9a31
Mayesha Rashid <1905103@ugrad.cse.buet.ac.bd>
2022-04-21 20:49:00 +0600

    Add solution for problem `C - Odd/Even Increments` Link: https://codeforces.com/contest/1669/problem/C Tags: greedy, greedy, implementation, math, *800 Ref: https://codeforces.com/contest/1669/submission/154320890

---
+++ codeforces/1669/C.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define fastio ios_base::sync_with_stdio(false);cin.tie(NULL)
+
+int main()
+{
+    fastio;
+    int t,n,i,ara[100],p;
+    cin>>t;
+    while(t--)
+    {
+        p=0;
+        cin>>n;
+        for(i=0;i<n;i++)
+            cin>>ara[i];
+        for(i=2;i<n;i++)
+        {
+            if(ara[i]%2 != ara[i-2]%2)
+            {
+                p=1;
+                break;
+            }
+        }
+        if(p==1)
+            cout<<"NO"<<"\n";
+        else
+            cout<<"YES"<<"\n";
+    }
+    return 0;
+}
