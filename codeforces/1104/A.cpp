8e09e2e08774d8eee981777dd902fdb441dc02f5
Mayesha Rashid <1905103@ugrad.cse.buet.ac.bd>
2022-10-17 19:59:00 +0600

    Add solution for problem `A - Splitting into digits` Link: https://codeforces.com/contest/1104/problem/A Tags: constructive algorithms, implementation, math, *800 Ref: https://codeforces.com/contest/1104/submission/176693179

---
+++ codeforces/1104/A.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+#include<string>
+using namespace std;
+#define fastio ios_base::sync_with_stdio(false);cin.tie(NULL)
+
+int main()
+{
+    fastio;
+    int n,i,k;
+    cin>>n;
+    cout<<n<<"\n";
+    for(i=0;i<n;i++)
+        cout<<1<<" ";
+    return 0;
+}
