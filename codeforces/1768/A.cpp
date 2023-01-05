e9cf3fcc3e6ff9d44ee12c02d74370436f62bf91
Mayesha Rashid <1905103@ugrad.cse.buet.ac.bd>
2023-01-05 20:40:00 +0600

    Add solution for problem `A - Greatest Convex` Link: https://codeforces.com/contest/1768/problem/A Tags: greedy, math, number theory, *800 Ref: https://codeforces.com/contest/1768/submission/188054012

---
+++ codeforces/1768/A.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+#include<string>
+using namespace std;
+#define fastio ios_base::sync_with_stdio(false);cin.tie(NULL)
+
+int main()
+{
+    fastio;
+    long long t,k;
+    cin>>t;
+    while(t--)
+    {
+        cin>>k;
+        cout<<k-1<<"\n";
+    }
+    return 0;
+}
