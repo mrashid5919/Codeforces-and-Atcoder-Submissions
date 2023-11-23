608b81a31e46af8355fce19825b23c6aadf580dc
Mayesha Rashid <1905103@ugrad.cse.buet.ac.bd>
2023-11-23 14:02:00 +0600

    Add solution for problem `A - Rating Goal` Link: https://atcoder.jp/contests/abc076/tasks/abc076_a Tags: AtCoder, *100 Ref: https://atcoder.jp/contests/abc076/submissions/47842728

---
+++ atcoder/abc076/A.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define fastio ios::sync_with_stdio(0);cin.tie(0)
+
+int main()
+{
+    fastio;
+    int a,b,dif;
+    cin>>a>>b;
+    dif=b-a;
+    cout<<a+2*dif<<"\n";
+    return 0;
+}
