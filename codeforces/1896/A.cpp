59b4a11343eaa099eccad6be1d1902e8cd769702
Mayesha Rashid <1905103@ugrad.cse.buet.ac.bd>
2023-11-25 21:16:00 +0600

    Add solution for problem `A - Jagged Swaps` Link: https://codeforces.com/contest/1896/problem/A Tags: sortings, *800 Ref: https://codeforces.com/contest/1896/submission/234265889

---
+++ codeforces/1896/A.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define fastio ios::sync_with_stdio(0);cin.tie(0)
+
+int main()
+{
+    fastio;
+    int t,n,i,p,ara[50];
+    cin>>t;
+    while(t--)
+    {
+        p=0;
+        cin>>n;
+        for(i=0;i<n;i++)
+            cin>>ara[i];
+        if(ara[0]==1)
+            cout<<"YES\n";
+        else
+            cout<<"NO\n";
+    }
+    return 0;
+}
