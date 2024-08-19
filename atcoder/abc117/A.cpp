274f959a2a4c4774ef82cf72d317ce3d872588ad
Mayesha Rashid <1905103@ugrad.cse.buet.ac.bd>
2024-08-19 22:03:00 +0600

    Add solution for problem `A - Entrance Examination` Link: https://atcoder.jp/contests/abc117/tasks/abc117_a Tags: AtCoder, *100 Ref: https://atcoder.jp/contests/abc117/submissions/56906122

---
+++ atcoder/abc117/A.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define fastio ios::sync_with_stdio(0);cin.tie(0)
+
+int main()
+{
+    fastio;
+    int t,x;
+    cin>>t>>x;
+    cout<<setprecision(6)<<(float)t/x<<"\n";
+    return 0;
+}
